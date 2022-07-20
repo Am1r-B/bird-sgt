8cde7f68d51235b5852d2fae1b1464611bf2437a
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-07-20 08:37:25 +0430

    make bird to fall down

--- main.lua
+++ main.lua
@@ -6,12 +6,19 @@ function love.load()
   birdY = 200
   birdWidth = 30
   birdHeight = 25
+  birdYSpeed = 30
+end
+
+function love.update(dt)
+  birdY = birdY + birdYSpeed * dt
 end
 
 function love.draw()
+  -- draw background
   love.graphics.setColor(.14, .36, .46)
   love.graphics.rectangle('fill', 0, 0, playingAreaWidth, playingAreaHeight)
   
+  -- draw bird
   love.graphics.setColor(.87, .84, .27)
   love.graphics.rectangle('fill', birdX, birdY, birdWidth, birdHeight)
 end
