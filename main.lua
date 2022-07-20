fc4a99bb7684810f09f5a570c2d8510635b5322a
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-07-20 08:34:57 +0430

    draw bird

--- main.lua
+++ main.lua
@@ -1,9 +1,17 @@
 function love.load()
   playingAreaWidth = 300
   playingAreaHeight = 388
+  
+  birdX = 62
+  birdY = 200
+  birdWidth = 30
+  birdHeight = 25
 end
 
 function love.draw()
   love.graphics.setColor(.14, .36, .46)
   love.graphics.rectangle('fill', 0, 0, playingAreaWidth, playingAreaHeight)
+  
+  love.graphics.setColor(.87, .84, .27)
+  love.graphics.rectangle('fill', birdX, birdY, birdWidth, birdHeight)
 end
